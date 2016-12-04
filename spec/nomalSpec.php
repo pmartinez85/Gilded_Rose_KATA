a8447b73a21f41c15abc1047ef02f93dd0a8d54a
Pedro Martínez <pmartinez1085@gmail.com>
2016-12-04 21:25:25 +0100

    copy oldtests into new files

--- spec/nomalSpec.php
+++ spec/nomalSpec.php
@@ -5,3 +5,30 @@
  * Date: 4/12/16
  * Time: 21:18
  */
+
+context ('normal Items', function () {
+    it ('updates normal items before sell date', function () {
+        $item = GildedRose::of('normal', 10, 5); // quality, sell in X days
+        $item->tick();
+        expect($item->quality)->toBe(9);
+        expect($item->sellIn)->toBe(4);
+    });
+    it ('updates normal items on the sell date', function () {
+        $item = GildedRose::of('normal', 10, 0);
+        $item->tick();
+        expect($item->quality)->toBe(8);
+        expect($item->sellIn)->toBe(-1);
+    });
+    it ('updates normal items after the sell date', function () {
+        $item = GildedRose::of('normal', 10, -5);
+        $item->tick();
+        expect($item->quality)->toBe(8);
+        expect($item->sellIn)->toBe(-6);
+    });
+    it ('updates normal items with a quality of 0', function () {
+        $item = GildedRose::of('normal', 0, 5);
+        $item->tick();
+        expect($item->quality)->toBe(0);
+        expect($item->sellIn)->toBe(4);
+    });
+});
--- spec/sulfuraSpec.php
+++ spec/sulfuraSpec.php
@@ -5,3 +5,24 @@
  * Date: 4/12/16
  * Time: 21:17
  */
+
+context('Sulfuras Items', function () {
+    it ('updates Sulfuras items before the sell date', function () {
+        $item = GildedRose::of('Sulfuras, Hand of Ragnaros', 10, 5);
+        $item->tick();
+        expect($item->quality)->toBe(10);
+        expect($item->sellIn)->toBe(5);
+    });
+    it ('updates Sulfuras items on the sell date', function () {
+        $item = GildedRose::of('Sulfuras, Hand of Ragnaros', 10, 5);
+        $item->tick();
+        expect($item->quality)->toBe(10);
+        expect($item->sellIn)->toBe(5);
+    });
+    it ('updates Sulfuras items after the sell date', function () {
+        $item = GildedRose::of('Sulfuras, Hand of Ragnaros', 10, -1);
+        $item->tick();
+        expect($item->quality)->toBe(10);
+        expect($item->sellIn)->toBe(-1);
+    });
+});
